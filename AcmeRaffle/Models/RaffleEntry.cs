d516b43120fb23bb23a8bc0eb2d33f52b1c07458
Mole <nikolajlauridsen@protonmail.ch>
2020-04-27 14:22:24 +0200

    Add inital models
    Further annotations will come

---
+++ AcmeRaffle/Models/RaffleEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AcmeRaffle.Models
+{
+    public class RaffleEntry
+    {
+        public int RaffleEntryId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public int SoldProductId { get; set; }
+        public SoldProduct SoldProduct { get; set; }
+    }
+}

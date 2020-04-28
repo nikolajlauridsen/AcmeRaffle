81f3b81e747c41711613b1ab30ee312e16f61d46
Mole <nikolajlauridsen@protonmail.ch>
2020-04-28 11:52:01 +0200

    Finish EntryValidator for now

--- RaffleLogic/Models/RaffleEntry.cs
+++ RaffleLogic/Models/RaffleEntry.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace RaffleLogic.Models
@@ -27,6 +28,7 @@ namespace RaffleLogic.Models
         public string Email { get; set; }
 
         public int SoldProductId { get; set; }
+
         public SoldProduct SoldProduct { get; set; }
     }
 }
--- RaffleLogic/Services/EntryValidator.cs
+++ RaffleLogic/Services/EntryValidator.cs
@@ -12,8 +12,15 @@ namespace RaffleLogic.Services
         public bool ValidateEntry(IQueryable<SoldProduct> products,
             IQueryable<RaffleEntry> entries, RaffleEntry entry)
         {
+            // Check if the serial number is valid
             if (products.Any(p => p.SerialNumber == entry.SoldProduct.SerialNumber))
             {
+                // Set the instance of SoldProduct in the entry to be the one from the database
+                // Otherwise the foreign key wont be resolved properly and the database will create a new product row
+                // Maybe move this to a separate function?
+                SoldProduct purchasedProduct = products.First(p => p.SerialNumber == entry.SoldProduct.SerialNumber);
+                entry.SoldProduct = purchasedProduct;
+                // Check that less than 2 entries has been submitted for the serial number
                 if (entries.Count(e => e.SoldProduct.SerialNumber == entry.SoldProduct.SerialNumber) < 2)
                 {
                     return true;

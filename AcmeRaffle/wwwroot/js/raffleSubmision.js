24f7fbed9fc78780bc7f22bec1d5fb1d938743a4
Mole <nikolajlauridsen@protonmail.ch>
2020-04-29 15:29:47 +0200

    Add javascript to its own file

---
+++ AcmeRaffle/wwwroot/js/raffleSubmision.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $('#submissionForm').submit(function (e) {
+        e.preventDefault();
+        $('#submitBtn').prop("disabled", true);
+        $('.overlay').show();
+        var data = {
+            'FirstName': $('#firstName').val(),
+            'LastName': $('#lastName').val(),
+            'Age': parseInt($('#age').val()),
+            'Email': $('#email').val(),
+            'SoldProduct': {
+                'SerialNumber': $('#serialNumber').val()
+            }
+        }
+        console.log(JSON.stringify(data));
+
+        $.ajax({
+            type: "POST",
+            url: '/api/RaffleApi',
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function (data) {
+                alert("Form successfully posted.");
+                $('#submitBtn').prop("disabled", false);
+                $('.overlay').hide();
+            },
+            error: function (e) {
+                alert("Form posting failed.");
+                $('#submitBtn').prop("disabled", false);
+                $('.overlay').hide();
+            }
+        });
+    });
+})

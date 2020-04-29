c3d88b33b249f12ed1dde61b42cd9997f20971cf
Mole <nikolajlauridsen@protonmail.ch>
2020-04-29 15:59:28 +0200

    Show status messages

--- AcmeRaffle/wwwroot/js/raffleSubmision.js
+++ AcmeRaffle/wwwroot/js/raffleSubmision.js
@@ -1,34 +1,59 @@
-﻿$(document).ready(function () {
-    $('#submissionForm').submit(function (e) {
+﻿$(document).ready(function() {
+    $('#submissionForm').submit(function(e) {
         e.preventDefault();
-        $('#submitBtn').prop("disabled", true);
-        $('.overlay').show();
-        var data = {
-            'FirstName': $('#firstName').val(),
-            'LastName': $('#lastName').val(),
-            'Age': parseInt($('#age').val()),
-            'Email': $('#email').val(),
-            'SoldProduct': {
-                'SerialNumber': $('#serialNumber').val()
-            }
-        }
-        console.log(JSON.stringify(data));
+        lockForm();
 
+        var data = packData();
         $.ajax({
             type: "POST",
             url: '/api/RaffleApi',
             data: JSON.stringify(data),
             contentType: "application/json",
-            success: function (data) {
-                alert("Form successfully posted.");
-                $('#submitBtn').prop("disabled", false);
-                $('.overlay').hide();
-            },
-            error: function (e) {
-                alert("Form posting failed.");
-                $('#submitBtn').prop("disabled", false);
-                $('.overlay').hide();
-            }
+            success: handleSuccess,
+            error: handleFailure
         });
     });
-})
+});
+
+function lockForm() {
+    $('#submitBtn').prop("disabled", true);
+    $('.overlay').show();
+}
+
+function unlockForm() {
+    $('#submitBtn').prop("disabled", false);
+    $('.overlay').hide();
+    $("#submissionForm").trigger('reset');
+}
+
+function packData() {
+    return {
+        'FirstName': $('#firstName').val(),
+        'LastName': $('#lastName').val(),
+        'Age': parseInt($('#age').val()),
+        'Email': $('#email').val(),
+        'SoldProduct': {
+            'SerialNumber': $('#serialNumber').val()
+        }
+    }
+}
+
+function handleSuccess(data) {
+    closeFailure();
+    $("#successAlert").show();
+    unlockForm();
+}
+
+function handleFailure(error) {
+    closeSuccess();
+    $("#failureAlert").show();
+    unlockForm();
+}
+
+function closeFailure() {
+    $("#failureAlert").hide();
+}
+
+function closeSuccess() {
+    $("#successAlert").hide();
+}

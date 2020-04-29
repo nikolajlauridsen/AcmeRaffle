fd21a9bc9016a48b783e3272c4156f9885819b71
Mole <nikolajlauridsen@protonmail.ch>
2020-04-29 16:17:53 +0200

    Tidy up javascript

--- AcmeRaffle/wwwroot/js/raffleSubmision.js
+++ AcmeRaffle/wwwroot/js/raffleSubmision.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function() {
-    $('#submissionForm').submit(function(e) {
+    $("#submissionForm").submit(function(e) {
         e.preventDefault();
         lockForm();
 
         var data = packData();
         $.ajax({
             type: "POST",
-            url: '/api/RaffleApi',
+            url: "/api/RaffleApi",
             data: JSON.stringify(data),
             contentType: "application/json",
             success: handleSuccess,
@@ -16,24 +16,24 @@
 });
 
 function lockForm() {
-    $('#submitBtn').prop("disabled", true);
-    $('.overlay').show();
+    $("#submitBtn").prop("disabled", true);
+    $(".overlay").show();
 }
 
 function unlockForm() {
-    $('#submitBtn').prop("disabled", false);
-    $('.overlay').hide();
-    $("#submissionForm").trigger('reset');
+    $("#submitBtn").prop("disabled", false);
+    $(".overlay").hide();
+    $("#submissionForm").trigger("reset");
 }
 
 function packData() {
     return {
-        'FirstName': $('#firstName').val(),
-        'LastName': $('#lastName').val(),
-        'Age': parseInt($('#age').val()),
-        'Email': $('#email').val(),
+        'FirstName': $("#firstName").val(),
+        'LastName': $("#lastName").val(),
+        'Age': parseInt($("#age").val()),
+        'Email': $("#email").val(),
         'SoldProduct': {
-            'SerialNumber': $('#serialNumber').val()
+            'SerialNumber': $("#serialNumber").val()
         }
     }
 }
